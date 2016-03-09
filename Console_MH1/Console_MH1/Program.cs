a43e33cc9bbdbc22bb614934056af997e9b29135
michael <michael.pegeot@gmail.com>
2016-03-09 15:21:57 +0100

    #1

---
+++ Console_MH1/Console_MH1/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Console_MH1
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    class Program
+    {
+        static void Main()
+        {
+
+
+            Console.ReadKey();
+        }
+    }
+}
