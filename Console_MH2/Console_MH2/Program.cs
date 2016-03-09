1e1496998a87ce1145689d21960c2e526da0cc16
michael <michael.pegeot@gmail.com>
2016-03-09 17:45:29 +0100

    33

---
+++ Console_MH2/Console_MH2/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Console_MH2
+{
+    using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            var a = 3;
+
+            Console.WriteLine(a);
+
+            Console.ReadKey();
+        }
+    }
+}
